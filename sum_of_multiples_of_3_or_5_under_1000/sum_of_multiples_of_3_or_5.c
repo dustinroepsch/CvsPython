9a7d6b81bead47687e1a829edefb47d734101e1e
Dustin Ryan - Roepsch <dustinroepsch@gmail.com>
2016-09-11 20:08:31 -0500

    Formmated C code

--- sum_of_multiples_of_3_or_5_under_1000/sum_of_multiples_of_3_or_5.c
+++ sum_of_multiples_of_3_or_5_under_1000/sum_of_multiples_of_3_or_5.c
@@ -1,13 +1,16 @@
-#include <stdio.h>
+# include <stdio.h>
 
 int main() {
-	int sum = 0;
-	int i;
-	for (i = 0; i < 1000; i++) {
-		if (i % 3 == 0 || i % 5 == 0) {
-			sum = sum + i;
-		}
-	}
-	printf("The sum of the natural numbers below 1000 that are multiples of 3 or 5 is %d!\n", sum);
-	return 0;
+    int sum = 0
+    int i
+    for (i=0
+         i < 1000
+         i + +) {
+        if (i % 3 == 0 | | i % 5 == 0) {
+            sum = sum + i
+        }
+    }
+    printf(
+        "The sum of the natural numbers below 1000 that are multiples of 3 or 5 is %d!\n", sum)
+    return 0
 }
