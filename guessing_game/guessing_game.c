606f00b1896d0f5074f28435de932ed515eb623b
Dustin Ryan - Roepsch <dustinroepsch@gmail.com>
2016-09-11 20:19:52 -0500

    Actually fix the formatting

--- guessing_game/guessing_game.c
+++ guessing_game/guessing_game.c
@@ -3,35 +3,35 @@
 #include <stdlib.h> //gives us the srand() and rand() functions
 
 int main() {
-	srand(time(NULL));
+    srand(time(NULL));
 
-	int done_playing = 0;// the program just started, so we obviously want to play;
+    int done_playing = 0;// the program just started, so we obviously want to play;
 
-	while (!done_playing) {
-		int secret_number = rand() % 100; //gives us a random number in the range [0,99] inclusive
-		int guess;
-		printf("I've picked a number between 0 and 99 inclusive, try to guess it!\n");
-		do {
-			printf("Enter a guess!\n");
-			scanf("%d", &guess);
-			if (guess < secret_number) {
-				printf("%d is too low!\n", guess);
-			} else if (guess > secret_number) {
-				printf("%d is too high!\n", guess);
-			} else {
-				printf("Congrats! %d was the correct number!\n", secret_number);
-			}
-		} while (guess != secret_number);
-		printf("Would you like to play again? y/n\n");
-		char play_again_choice;
-		scanf(" %c", &play_again_choice);
-		if (play_again_choice == 'n' || play_again_choice == 'N') {
-			done_playing = 1;
-		}
-	}
+    while (!done_playing) {
+        int secret_number = rand() % 100; //gives us a random number in the range [0,99] inclusive
+        int guess;
+        printf("I've picked a number between 0 and 99 inclusive, try to guess it!\n");
+        do {
+            printf("Enter a guess!\n");
+            scanf("%d", &guess);
+            if (guess < secret_number) {
+                printf("%d is too low!\n", guess);
+            } else if (guess > secret_number) {
+                printf("%d is too high!\n", guess);
+            } else {
+                printf("Congrats! %d was the correct number!\n", secret_number);
+            }
+        } while (guess != secret_number);
+        printf("Would you like to play again? y/n\n");
+        char play_again_choice;
+        scanf(" %c", &play_again_choice);
+        if (play_again_choice == 'n' || play_again_choice == 'N') {
+            done_playing = 1;
+        }
+    }
 
-	printf("Goodbye!\n");
+    printf("Goodbye!\n");
 
-	return 0;
+    return 0;
 }
 
--- hello_world/hello_world.c
+++ hello_world/hello_world.c
@@ -1,6 +1,6 @@
 #include <stdio.h> //this gives the ability to use printf
 
 int main() {
-	printf("Hello World!\n"); // \n is the "new line" character, it's the same thing as hitting "enter" on your keyboard while typing.
-	return 0; //tells the operating system that the program ran successfully
+    printf("Hello World!\n"); // \n is the "new line" character, it's the same thing as hitting "enter" on your keyboard while typing.
+    return 0; //tells the operating system that the program ran successfully
 }
