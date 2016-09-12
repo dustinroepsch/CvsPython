a394a61f790d7ba8ab6b6c82f1b87ee1d1d646b7
Dustin Ryan-Roepsch <dustinroepsch@gmail.com>
2016-09-12 14:51:06 -0500

    formatted c

--- disemvoweler/disemvoweler2.c
+++ disemvoweler/disemvoweler2.c
@@ -3,39 +3,39 @@
 #include<stdlib.h>
 
 /* Lowercases all letters in str */
-void toLower(char* str){
-   int i, length;
+void toLower(char* str) {
+    int i, length;
 
-   length = strlen(str);
-   for (i = 0; i < length; ++i)
-      if (str[i] >= 'A' && str[i] <= 'Z')
-         str[i] += 'a' - 'A';
+    length = strlen(str);
+    for (i = 0; i < length; ++i)
+        if (str[i] >= 'A' && str[i] <= 'Z')
+            str[i] += 'a' - 'A';
 }
 
 /* Returns allocated string */
-char* removeAll(char* original, char* toRemove){
-   int original_length, remove_length, new_length;
-   int i, j;
-   char* result;
+char* removeAll(char* original, char* toRemove) {
+    int original_length, remove_length, new_length;
+    int i, j;
+    char* result;
 
-   original_length = strlen(original);
-   remove_length = strlen(toRemove);
-   result = calloc(original_length, 1);
+    original_length = strlen(original);
+    remove_length = strlen(toRemove);
+    result = calloc(original_length, 1);
 
-   new_length = 0;
-   for (i = 0; i < original_length; ++i){
-      for (j = 0; j < remove_length; ++j){
-         if (original[i] == toRemove[j])
-            break;
-      }
-      if (j == remove_length){
-         result[new_length] = original[i];
-         ++new_length;
-      }
-   }
+    new_length = 0;
+    for (i = 0; i < original_length; ++i) {
+        for (j = 0; j < remove_length; ++j) {
+            if (original[i] == toRemove[j])
+                break;
+        }
+        if (j == remove_length) {
+            result[new_length] = original[i];
+            ++new_length;
+        }
+    }
 
-   result[new_length] = 0;
-   return result;
+    result[new_length] = 0;
+    return result;
 }
 
 int main(void) {
